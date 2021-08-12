426f12df5cbc8c5758e2dfa2365fb089b706ed6d
Mathias Lundell <lundell.mathias@gmail.com>
2021-08-12 14:05:15 +0200

    more types stuff

--- types/mq.d.ts
+++ types/mq.d.ts
@@ -1,3 +1,10 @@
+import { ChannelWrapper } from 'amqp-connection-manager'
+import { Message } from './message'
+
+export interface HandleOptions {
+  handler: (message: Message) => Promise<void>
+}
+
 export default class Mq2 {
     constructor({ topology, logger, unblockTimeout, reconnectTimeout, reconnectTime, unhandledTimeout, requestTimeout }: {
         topology: any;
@@ -8,38 +15,15 @@ export default class Mq2 {
         unhandledTimeout?: number;
         requestTimeout?: number;
     });
-    logger: any;
-    topology: any;
-    heartbeat: any;
-    unhandledTimeout: number;
-    requestTimeout: number;
-    unblockTimeout: any;
-    reconnectTimeout: any;
-    reconnectTimeInSeconds: any;
-    requests: any[];
-    _consumerUnsubscribed: boolean;
     configure(): Promise<void>;
-    publishConnection: any;
-    publishChannel: any;
-    consumeConnection: any;
-    consumeChannel: any;
     shutdown(): Promise<void>;
     close(): Promise<void>;
     unsubscribeAll(): Promise<void>;
-    handle(options: any): Promise<void>;
+    handle(options: HandleOptions): Promise<void>;
     publish(exchange: any, { body, routingKey, headers }: {
         body: any;
         routingKey: any;
         headers: any;
     }, options?: {}): Promise<void>;
     request(exchange: any, message: any, options: any): Promise<any>;
-    _handleMessage(message: any, { handler }: {
-        handler: any;
-    }): Promise<void>;
-    _publish(exchange: any, routingKey: any, content: any, options: any): Promise<void>;
-    _onReply(message: any): Promise<void>;
-    _createConnection(urlObj: any, socketOptions: any): Promise<any>;
-    _createPublishChannel(connection: any, topology: any): Promise<any>;
-    _createConsumeChannel(connection: any, topology: any): Promise<any>;
-    _setupTopology(channel: any, topology: any, ...args: any[]): Promise<void>;
 }
