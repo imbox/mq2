5c8a671602bfb6e0f17436c881c0641fe41e1ee6
Mathias Lundell <lundell.mathias@gmail.com>
2021-08-13 15:47:35 +0200

    initial tests

--- test/fixtures.js
+++ test/fixtures.js
@@ -1,22 +1,52 @@
+'use strict'
+
 const { EventEmitter } = require('events')
 
-class FakeChannelWrapper extends EventEmitter {}
+class FakeChannelWrapper extends EventEmitter {
+  constructor () {
+    super()
+    this.published = []
+  }
+
+  async publish (exchange, routingKey, content, options) {
+    this.published.push({ exchange, routingKey, content, options })
+    return false
+  }
+}
 
 class FakeConnectionWrapper extends EventEmitter {
-  createChannel (...opts) {
-    return new FakeChannelWrapper(...opts)
+  constructor () {
+    super()
+    this.channel = null
+  }
+
+  createChannel (...args) {
+    this.channel = new FakeChannelWrapper(...args)
+    setImmediate(() => {
+      this.channel.emit('connect')
+    })
+    return this.channel
+  }
+}
+
+class FakeAmqpConnectionManager {
+  constructor () {
+    this.connections = []
+  }
+
+  connect (...args) {
+    const connection = new FakeConnectionWrapper(...args)
+    this.connections.push(connection)
+
+    setImmediate(() => {
+      connection.emit('connect')
+    })
+    return connection
   }
 }
 
 module.exports = {
-  fakeAmqpConnectionManager: {
-    connect (...opts) {
-      const connection = new FakeConnectionWrapper(...opts)
-      setImmediate(() => {
-        connection.emit('connect')
-      })
-
-      return connection
-    }
-  }
+  FakeAmqpConnectionManager,
+  FakeConnectionWrapper,
+  FakeChannelWrapper
 }
--- test/mq.test.js
+++ test/mq.test.js
@@ -1,10 +1,9 @@
-const proxyquire = require('proxyquire')
-const { test } = require('tap')
-const { fakeAmqpConnectionManager } = require('./fixtures')
+'use strict'
 
-const Mq = proxyquire('../lib/mq', {
-  'amqp-connection-manager': fakeAmqpConnectionManager
-})
+const proxyquire = require('proxyquire').noCallThru()
+const { test } = require('tap')
+const { FakeAmqpConnectionManager } = require('./fixtures')
+const { Mq } = require('../')
 
 test('Mq', t => {
   t.plan(1)
@@ -12,6 +11,22 @@ test('Mq', t => {
 })
 
 test('configure', async () => {
+  const Mq = proxyquire('../lib/mq', {
+    'amqp-connection-manager': new FakeAmqpConnectionManager()
+  })
   const mq = new Mq({ topology: { connection: {} } })
   await mq.configure()
+})
+
+test('publish', async () => {
+  const Mq = proxyquire('../lib/mq', {
+    'amqp-connection-manager': new FakeAmqpConnectionManager()
+  })
+
+  const mq = new Mq({ topology: { connection: {} } })
+  await mq.configure()
+  await mq.publish('exchange', {
+    routingKey: 'routingkey',
+    body: { test: 'test' }
+  })
 })
