2bf6703675635dab18e3a4c7f41420c2cfffc562
Mathias Lundell <lundell.mathias@gmail.com>
2021-08-23 07:53:24 +0200

    test: some more tests
    I want to replace these test with simple-amqp-server when it is
    ready.

--- test/mq.test.js
+++ test/mq.test.js
@@ -10,23 +10,46 @@ test('Mq', t => {
   t.doesNotThrow(() => new Mq({ topology: { connection: {} } }))
 })
 
-test('configure', async () => {
+test('configure', async t => {
+  const fakeAmqp = new FakeAmqpConnectionManager()
   const Mq = proxyquire('../lib/mq', {
-    'amqp-connection-manager': new FakeAmqpConnectionManager()
+    'amqp-connection-manager': fakeAmqp
   })
   const mq = new Mq({ topology: { connection: {} } })
   await mq.configure()
+  t.equal(fakeAmqp.connections.length, 2)
 })
 
-test('publish', async () => {
+test('publish', async t => {
+  const fakeAmqp = new FakeAmqpConnectionManager()
   const Mq = proxyquire('../lib/mq', {
-    'amqp-connection-manager': new FakeAmqpConnectionManager()
+    'amqp-connection-manager': fakeAmqp
   })
-
   const mq = new Mq({ topology: { connection: {} } })
   await mq.configure()
+
+  const body = { test: 'test' }
   await mq.publish('exchange', {
-    routingKey: 'routingkey',
-    body: { test: 'test' }
+    routingKey: 'routingKey',
+    body
   })
+
+  const published = fakeAmqp.connections.flatMap(c => c.channel.published)
+  t.equal(published.length, 1)
+
+  t.equal(published[0].options.correlationId.length, 36)
+  delete published[0].options.correlationId
+
+  t.strictSame(published, [
+    {
+      exchange: 'exchange',
+      routingKey: 'routingKey',
+      content: Buffer.from(JSON.stringify(body)),
+      options: {
+        headers: undefined,
+        contentEncoding: 'utf8',
+        contentType: 'application/json'
+      }
+    }
+  ])
 })
