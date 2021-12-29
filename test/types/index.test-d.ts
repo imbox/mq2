39a8f6dea717c6fdd2939003c6fcfbbc4feab557
Mathias Lundell <lundell.mathias@gmail.com>
2021-12-29 16:58:53 +0100

    test(types): verify topology

--- test/types/index.test-d.ts
+++ test/types/index.test-d.ts
@@ -4,7 +4,45 @@ import DefaultMq, { Mq, Message, Request } from '../../'
 const mq = new Mq({
   logger: console,
   topology: {
-    connection: {}
+    connection: {
+      host: 'localhost',
+      port: 5672,
+      user: 'guest',
+      pass: 'guest',
+      heartbeat: 10
+    },
+    socketOptions: {
+      clientProperties: {
+        something: 'something'
+      }
+    },
+    exchanges: [
+      {
+        name: 'exchange-name',
+        type: 'topic',
+        durable: true,
+        autoDelete: false,
+        arguments: {
+          custom: 'arguments'
+        }
+      }
+    ],
+    queues: [
+      {
+        name: 'queue-name',
+        durable: true,
+        autoDelete: false
+      }
+    ],
+    bindings: [
+      {
+        exchange: 'exchange-name',
+        target: 'queue-name',
+        keys: [
+          'matching.routingkey.*'
+        ]
+      }
+    ]
   }
 })
 
